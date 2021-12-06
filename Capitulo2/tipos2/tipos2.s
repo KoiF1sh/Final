9a791afaf4b7c369584e91c3278762508b9bb5c6
KoiF1sh <89399746+KoiF1sh@users.noreply.github.com>
2021-12-06 15:10:51 -0800

    Create tipos2.s

---
+++ Capitulo2/tipos2/tipos2.s
@@ -0,0 +1,19 @@
+/* Problema tipos2.s
+	Traducir el programa de la estructura del if de C a ensamblador del libro de practicas
+*/
+
+//@Autor    KoiF1sh    
+//@Fecha    6 Dic 2021
+
+.text
+  .global main
+main:
+  push    {r4, lr}
+  ldr     r0, .L3
+  bl      puts
+  mov     r0, #0
+  pop     {r4, pc}
+.L3:
+  .word   .LC0
+.LC0:
+  .ascii  "Se cumple\000"
