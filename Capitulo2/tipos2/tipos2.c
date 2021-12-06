79cd88572331d419d4ee3ef46cc394f9dd849acf
KoiF1sh <89399746+KoiF1sh@users.noreply.github.com>
2021-12-06 15:09:53 -0800

    Create tipos2.c

---
+++ Capitulo2/tipos2/tipos2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/* Problema tipos2.c
+	Escribir el programa de la estructura del if en C del libro de practicas y modificarlo
+*/
+
+//@Autor    KoiF1sh    
+//@Fecha    6 Dic 2021
+
+int main()
+{
+	int a = 5, b = 5;
+	if (a == b) 
+	{
+		/* Codigo entonces */
+		printf("Se cumple\n");
+	}
+	else 
+	{
+		/* Codigo sino */
+		printf("No se cumple\n");
+	}
+	return 0;
+}
