6b7a1d9868b4127f9c790f3bdefc2af5c9a87702
KoiF1sh <89399746+KoiF1sh@users.noreply.github.com>
2021-12-06 15:16:39 -0800

    Update tipos1.s

--- Capitulo2/tipos1/tipos1.s
+++ Capitulo2/tipos1/tipos1.s
@@ -5,30 +5,30 @@
 //@Autor    KoiF1sh    
 //@Fecha    6 Dic 2021
 
- .text
-  .global main
+.text
+.global main
 main:
-  push    {r4, r5, r6, lr}
-  mov     r4, #6
-  ldr     r5, .L7
+	push    {r4, r5, r6, lr}
+	mov     r4, #6
+	ldr     r5, .L7
 .L2:
-  mov     r0, r5
-  bl      printf
-  subs    r4, r4, #1
-  bne     .L2
-  ldr     r5, .L7+4
-  mov     r4, #6
+	mov     r0, r5
+	bl      printf
+	subs    r4, r4, #1
+	bne     .L2
+	ldr     r5, .L7+4
+	mov     r4, #6
 .L3:
-  mov     r0, r5
-  bl      printf
-  subs    r4, r4, #1
-  bne     .L3
-  mov     r0, r4
-  pop     {r4, r5, r6, pc}
+	mov     r0, r5
+	bl      printf
+	subs    r4, r4, #1
+	bne     .L3
+	mov     r0, r4
+	pop     {r4, r5, r6, pc}
 .L7:
-  .word   .LC0
-  .word   .LC1
+	.word   .LC0
+	.word   .LC1
 .LC0:
-  .ascii  "for \000"
+	.ascii  "for \000"
 .LC1:
-  .ascii  "while \000"
+	.ascii  "while \000"
