690d697a36f8b1e8aed402cf4431e46462e11cee
KoiF1sh <89399746+KoiF1sh@users.noreply.github.com>
2021-12-06 15:31:06 -0800

    Update tipos3.c

--- Capitulo2/tipos3/tipos3.c
+++ Capitulo2/tipos3/tipos3.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+/* Problema tipos3.c
+	Escribir el código del programa tipos3.c
+*/
+
+//@Autor    KoiF1sh    
+//@Fecha    6 Dic 2021
+
+void main(void)
+{
+	int i;
+	for (i = 0; i < 5; i++) {
+		printf(" %d\n ", i);
+	}
+}
