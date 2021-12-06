e61e31ae2fae1b6fef0a9f4e60b5272e4d3e78e9
KoiF1sh <89399746+KoiF1sh@users.noreply.github.com>
2021-12-06 15:25:45 -0800

    Update tipos2.s

--- Capitulo2/tipos2/tipos2.s
+++ Capitulo2/tipos2/tipos2.s
@@ -6,14 +6,14 @@
 //@Fecha    6 Dic 2021
 
 .text
-  .global main
+	.global main
 main:
-  push    {r4, lr}
-  ldr     r0, .L3
-  bl      puts
-  mov     r0, #0
-  pop     {r4, pc}
+	push    {r4, lr}
+	ldr     r0, .L3
+	bl      puts
+	mov     r0, #0
+	pop     {r4, pc}
 .L3:
-  .word   .LC0
+	.word   .LC0
 .LC0:
-  .ascii  "Se cumple\000"
+	.ascii  "Se cumple\000"
