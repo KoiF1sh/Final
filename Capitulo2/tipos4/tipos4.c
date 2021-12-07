f45a8b775c750ab9c7b588b4569a154e3f4c91c4
KoiF1sh <89399746+KoiF1sh@users.noreply.github.com>
2021-12-06 16:07:00 -0800

    Update tipos4.c

--- Capitulo2/tipos4/tipos4.c
+++ Capitulo2/tipos4/tipos4.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+/* Problema tipos4.c
+	Escribir el programa de la suma de elementos de un vector en c
+*/
+
+//@Autor    KoiF1sh    
+//@Fecha    6 Dic 2021
+
+void main ( void )
+{
+int i, suma;
+int vector [5]= {128 , 32 , 100 , -30 , 124};
+
+  for ( suma = i = 0; i <5; i ++ )
+  {
+    suma += vector [i ];
+  }
+  
+printf (" La suma es %d \n" , suma );
+}
