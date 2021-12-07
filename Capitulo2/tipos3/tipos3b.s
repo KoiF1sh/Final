54a9207d1918e28b59c24bc5415a13a6fe97b611
KoiF1sh <89399746+KoiF1sh@users.noreply.github.com>
2021-12-06 18:50:52 -0800

    Update tipos3b.s

--- Capitulo2/tipos3/tipos3b.s
+++ Capitulo2/tipos3/tipos3b.s
@@ -1,27 +1,34 @@
+/* Problema tipos3a.s
+	Escribir el código del programa tipos3a.c
+*/
+
+//@Autor    KoiF1sh    
+//@Fecha    6 Dic 2021
+
 .data
 var1 : .asciz " %d\ 012 "
 .text
 .global main
-main : push { r4, lr }
-mov r1, # 0
-ldr r4, = var1
-mov r0, r4
-bl printf
-mov r0, r4
-mov r1, # 1
-bl printf
-mov r0, r4
-mov r1, # 2
-bl printf
-mov r0, r4
-mov r1, # 3
-bl printf
-mov r0, r4
-mov r1, # 4
-pop { r4, lr }
-b printf
-mov r0, r4
-mov r1, # 4
-bl printf
-pop { r4, lr }
-bx lr
+main :  push { r4, lr }
+  mov r1, # 0
+  ldr r4, = var1
+  mov r0, r4
+  bl printf
+  mov r0, r4
+  mov r1, # 1
+  bl printf
+  mov r0, r4
+  mov r1, # 2
+  bl printf
+  mov r0, r4
+  mov r1, # 3
+  bl printf
+  mov r0, r4
+  mov r1, # 4
+  pop { r4, lr }
+  b printf
+  mov r0, r4
+  mov r1, # 4
+  bl printf
+  pop { r4, lr }
+  bx lr
