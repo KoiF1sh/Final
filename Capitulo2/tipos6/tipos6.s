830eb36b08f33642945c0e0d343b5c2508f72182
KoiF1sh <89399746+KoiF1sh@users.noreply.github.com>
2021-12-06 18:25:54 -0800

    Update tipos6.s

--- Capitulo2/tipos6/tipos6.s
+++ Capitulo2/tipos6/tipos6.s
@@ -1,5 +1,3 @@
-#include <stdio.h>
-
 /* Problema tipos6.s
 	Escribir el programa de la suma de un vector de enteros largos
 */
@@ -15,26 +13,26 @@ var2 : .word 1600000000, - 100, 800000000, - 50, 200
 .global main
 
 /* Salvamos registros */
-main :  push { r4, r5, r6, lr }
+main :	push { r4, r5, r6, lr }
 
 /* Inicializamos variables y apuntamos r4 a var2 */
-  mov r5, # 5
-  mov r2, # 0
-  mov r3, # 0
-  ldr r4, = var2
+	mov r5, # 5
+	mov r2, # 0
+	mov r3, # 0
+	ldr r4, = var2
 
 /* Bucle que hace la suma */
-bucle : ldr r0, [ r4 ] , # 4
-  mov r1, r0, ASR # 31
-  adds r2, r2, r0
-  adc r3, r3, r1
-  subs r5, r5, #1
-  bne bucle
+bucle :	ldr r0, [ r4 ] , # 4
+	mov r1, r0, ASR # 31
+	adds r2, r2, r0
+	adc r3, r3, r1
+	subs r5, r5, #1
+	bne bucle
 
 /* Imprimimos resultado */
-  ldr r0, = var1
-  bl printf
+	ldr r0, = var1
+	bl printf
 
 /* Recuperamos registros y salimos */
-  pop { r4, r5, r6, lr }
-  bx lr
+	pop { r4, r5, r6, lr }
+	bx lr
