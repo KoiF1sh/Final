624dcc76e698b59cf13ee94b55a740d550befacb
KoiF1sh <89399746+KoiF1sh@users.noreply.github.com>
2021-12-08 11:15:58 -0800

    Create subrut1.c

---
+++ Capitulo3/subrut1/subrut1.c
@@ -0,0 +1,28 @@
+# include <stdio.h>
+
+/* Problema subrut1.c
+	Escribir el programa del Código del programa subrut1 en c
+*/
+
+//@Autor    KoiF1sh    
+//@Fecha    9 Dic 2021
+
+static int seed;
+short myrand(void)
+{
+seed = seed*1103515245 + 12345;
+return seed>>16 & 0x7fff;
+}
+void mysrand ( int x ){
+seed = x;
+}
+
+void main(void)
+{
+int i;
+mysrand (42);
+for ( i= 0; i <5; i ++ )
+{
+printf ( " %d\n " , myrand ());
+}
+}
