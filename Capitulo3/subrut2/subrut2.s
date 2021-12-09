c0205e679c372b21202491e325a4dcfeef1a8d35
KoiF1sh <89399746+KoiF1sh@users.noreply.github.com>
2021-12-09 06:44:12 -0800

    Update subrut2.s

--- Capitulo3/subrut2/subrut2.s
+++ Capitulo3/subrut2/subrut2.s
@@ -1,7 +1,7 @@
 /* Problema 3.5: subrut2.s
 	Escribir la traduccion del programa del Código del programa subrut2 en s
 */
-// Instito tecnológico de Tijuana
+// Instituto tecnológico de Tijuana
 // Lenguaje de interfaz
 //@Autor    KoiF1sh    
 //@Fecha    9 Dic 2021
