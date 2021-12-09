d437bc98366653151442f1b5aa2b7c0bc7a82573
KoiF1sh <89399746+KoiF1sh@users.noreply.github.com>
2021-12-09 15:09:07 -0800

    Update subrut5.s

--- Capitulo3/subrut5/subrut5.s
+++ Capitulo3/subrut5/subrut5.s
@@ -1,11 +1,14 @@
 .data
-var1 : .asciz " %d\ n"
+var1 : .asciz "%d\n"
+
 .text
 .global main
+
 /* Salvo registros */
-main : push { r4, lr }
+main: push  {r4, lr}
+
 /* Introduzco los 4 primeros par á metros vía registros */
-mov r0, # 1
+  mov r0, # 1
 mov r1, # 2
 mov r2, # 3
 mov r3, # 4
