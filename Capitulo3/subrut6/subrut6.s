7245e81a4413f0ef82ca773b0ac07f19cfbe37d5
KoiF1sh <89399746+KoiF1sh@users.noreply.github.com>
2021-12-09 17:55:19 -0800

    Update subrut6.s

--- Capitulo3/subrut6/subrut6.s
+++ Capitulo3/subrut6/subrut6.s
@@ -1,1 +1,71 @@
+/* Problema 3.12: subrut5.s
+	Escribir el código del programa subrut5 en s
+*/
+// Instituto tecnológico de Tijuana
+// Lenguaje de interfaz
+//@Autor    KoiF1sh    
+//@Fecha    9 Dic 2021
 
+.data
+var1: .asciz "%d\n"
+
+.text
+.global main
+
+/* Salvo registros */
+main: push  {r4, lr}
+
+/* Introduzco los 4 primeros parámetros vía registros */
+	mov r0, #1
+	mov r1, #2
+	mov r2, #3
+	mov r3, #4
+
+/* Introduzco el 5o parámetro por pila */
+	mov r4, #5
+	push { r4 }
+
+/* Llamada a función poly3 (1, 2, 3, 4, 5) */
+	bl poly3
+
+/* Equilibro la pila ( debido al 5o par á metro ) */
+	add sp, #4
+
+/* Paso resultado de la función a r1, cadena a
+imprimir a r0 y llamo a la función */
+	mov r1, r0
+	ldr r0, =var1
+	bl printf
+
+/* Segunda llamada, esta vez poly3 (1, -1, 1, -1, 8) */
+	mov r0, #1
+	mov r1, #-1
+	mov r2, #1
+	mov r3, #-1
+	mov r4, #8
+	push {r4}
+	bl poly3
+	add sp, #4
+
+/* Imprimo resultado de segunda llamada */
+	mov r1, r0
+	ldr r0, =var1
+	bl printf
+
+/* Llamo e imprimo poly3 (2, 0, 0, 0, 8) */
+	mov r0, #2
+	mov r1, #0
+	mov r2, #0
+	mov r3, #0
+	mov r4, #8
+	push {r4}
+	bl poly3
+	add sp, #4
+	mov r1, r0
+	ldr r0, =var1
+	bl printf
+
+/* Recupero registros y salgo de main */
+	pop	{r4, lr}
+	bx lr
+	.equ param5, 4 *1 /* r4 */
