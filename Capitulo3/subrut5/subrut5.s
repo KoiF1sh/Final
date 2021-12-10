3b7aae269d96269064facb539172496a4a8cba43
KoiF1sh <89399746+KoiF1sh@users.noreply.github.com>
2021-12-09 17:39:53 -0800

    Update subrut5.s

--- Capitulo3/subrut5/subrut5.s
+++ Capitulo3/subrut5/subrut5.s
@@ -1,5 +1,5 @@
 .data
-var1 : .asciz "%d\n"
+var1: .asciz "%d\n"
 
 .text
 .global main
@@ -58,15 +58,15 @@ imprimir a r0 y llamo a la función */
 	bl printf
 
 /* Recupero registros y salgo de main */
-    pop	{r4, lr}
-    bx lr
-    .equ param5, 4 *1 /* r4 */
+	pop	{r4, lr}
+	bx lr
+	.equ param5, 4 *1 /* r4 */
 poly3: push {r4}		@ salvaguarda r4
-    ldr r4, [sp, #param5] 	@ leo r4 de pila
-    smlabb r3, r2, r4, r3 	@ r3= c*x + d
-    smulbb r2, r4, r4 		@ r2= x*x
-    smlabb r3, r1, r2, r3 	@ r3= b*(x*x) + (c*x + d)
-    smulbb r2, r2, r4 		@ r2= x*(x*x)
-    smlabb r0, r0, r2, r3 	@ r0= a*x*x*x + b*x*x + c*x+d
-    pop {r4} 			@ recupero r4
-    bx lr 			@ salgo de la función
+	ldr r4, [sp, #param5] 	@ leo r4 de pila
+	smlabb r3, r2, r4, r3 	@ r3= c*x + d
+	smulbb r2, r4, r4 		@ r2= x*x
+	smlabb r3, r1, r2, r3 	@ r3= b*(x*x) + (c*x + d)
+	smulbb r2, r2, r4 		@ r2= x*(x*x)
+	smlabb r0, r0, r2, r3 	@ r0= a*x*x*x + b*x*x + c*x+d
+	pop {r4} 			@ recupero r4
+	bx lr 			@ salgo de la función
