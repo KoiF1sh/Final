f48f5ec52ca4b4695ff93a128c4e91f3bb77263a
KoiF1sh <89399746+KoiF1sh@users.noreply.github.com>
2021-12-12 08:47:38 -0800

    Update esbn2.s

--- Capitulo4/esbn2/esbn2.s
+++ Capitulo4/esbn2/esbn2.s
@@ -1,5 +1,5 @@
-/* Problema 4.1: esbn1.s
-	Escribir el programa esbn1.s que trata sobre el funcionamiento de una llamada a printf
+/* Problema 4.2: esbn2.s
+	Escribir el programa bare metal esbn2.s que trata sobre el encendido de un LED (GPIO 9)
 */
 //Instituto tecnológico de Tijuana
 //Lenguaje de interfaz
@@ -7,15 +7,17 @@
 //@Autor	KoiF1sh    
 //@Fecha	12 Dic 2021
 
-.set	GPBASE, 0x20200000
+.set	GPBASE,	0x20200000
 .set	GPFSEL0, 0x00
-.set	GPSET0, 0x1c
+.set	GPSET0,	0x1c
 .text
-	ldr r0, = GPBASE
+
+	ldr	r0, =GPBASE
 /* guia bits xx999888777666555444333222111000 */
-	mov r1, # 0b00001000000000000000000000000000
-	str r1, [ r0, # GPFSEL0 ] @ Configura GPIO 9
+	mov	r1, #0b00001000000000000000000000000000
+	str	r1, [r0, #GPFSEL0] 				@ Configura GPIO 9
+	
 /* guia bits 10987654321098765432109876543210 */
-	mov r1, # 0b00000000000000000000001000000000
-	str r1, [ r0, # GPSET0 ] @ Enciende GPIO 9
-infi: 	b infi
+	mov	r1, #0b00000000000000000000001000000000
+	str	r1, [r0, #GPSET0] 				@ Enciende GPIO 9
+infi:	b	infi
