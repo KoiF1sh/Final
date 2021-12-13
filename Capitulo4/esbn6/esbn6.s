31ffcbf57940918a539284d8341e1ebdb1bb1da0
KoiF1sh <89399746+KoiF1sh@users.noreply.github.com>
2021-12-12 16:51:08 -0800

    Update esbn6.s

--- Capitulo4/esbn6/esbn6.s
+++ Capitulo4/esbn6/esbn6.s
@@ -1,5 +1,5 @@
-/* Problema 4.3: esbn3.s
-	Escribir el programa esbn3.s que trata sobre un LED parpadeante con bucle de retardo
+/* Problema 4.6: esbn6.s
+	Escribir el programa esbn6.s que trata sobre 
 */
 //Instituto tecnológico de Tijuana
 //Lenguaje de interfaz
@@ -9,21 +9,21 @@
 
 	ldr r0, = GPBASE
 /* guia bits xx999888777666555444333222111000 */
-mov r1, # 0b00000000000000000001000000000000
-str r1, [ r0, # GPFSEL0 ] @ Configura GPIO 4
+	mov r1, #0b00000000000000000001000000000000
+	str r1, [r0, #GPFSEL0] 				@ Configura GPIO 4
 /* guia bits 10987654321098765432109876543210 */
-mov r1, # 0b00000000000000000000000000010000
-ldr r2, = STBASE
-bucle : bl espera @ Salta a rutina de espera
-str r1, [ r0, # GPSET0 ]
-bl espera @ Salta a rutina de espera
-str r1, [ r0, # GPCLR0 ]
-b bucle
+	mov r1, #0b00000000000000000000000000010000
+	ldr r2, =STBASE
+bucle: bl espera 					@ Salta a rutina de espera
+	str r1, [r0, #GPSET0]
+	bl espera 					@ Salta a rutina de espera
+	str r1, [r0, #GPCLR0]
+	b bucle
 /* rutina que espera 1136 microsegundos */
-espera : ldr r3, [ r2, # STCLO ] @ Lee contador en r3
-ldr r4, = 1136
-add r4, r3 @ r4= r3 + 1136
-ret1 : ldr r3, [ r2, # STCLO ]
-cmp r3, r4 @ Leemos CLO hasta alcanzar
-bne ret1 @ el valor de r4
-bx lr
+espera: ldr r3, [r2, #STCLO] 				@ Lee contador en r3
+	ldr r4, =1136
+	add r4, r3 					@ r4= r3 + 1136
+ret1: ldr r3, [r2, #STCLO]
+	cmp r3, r4 					@ Leemos CLO hasta alcanzar
+	bne ret1 					@ el valor de r4
+	bx lr
