4dfb65332fcc576fd968cf9db40a582849f16559
KoiF1sh <89399746+KoiF1sh@users.noreply.github.com>
2021-12-12 17:02:08 -0800

    Update esbn4.s

--- Capitulo4/esbn4/esbn4.s
+++ Capitulo4/esbn4/esbn4.s
@@ -18,6 +18,7 @@
 	str r1, [r0, #GPFSEL0] 				@ Configura GPIO 9
 /* guia bits 10987654321098765432109876543210 */
 	mov r1, #0b00000000000000000000001000000000
+
 bucle: ldr r2, =7000000
 ret1: subs r2, #1 					@ Bucle de retardo 1
 	bne ret1
@@ -26,4 +27,5 @@ ret1: subs r2, #1 					@ Bucle de retardo 1
 ret2: subs r2, #1 					@ Bucle de retardo 2
 	bne ret2
 	str r1, [r0, #GPCLR0] 				@ Enciende el LED
+
 	b bucle 					@ Repetir para siempre
